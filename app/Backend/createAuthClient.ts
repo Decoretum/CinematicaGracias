c4a2ba01e871129e94e2b83b2fc7a2c91940de7f
Gael Estrera <gaelthegoodboy@gmail.com>
2025-11-03 05:26:56 +0800

    refactor(backend): Added supabase service role key in private production env file

--- app/Backend/createAuthClient.ts
+++ app/Backend/createAuthClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const authClient = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string, {
+export const authClient = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.SUPABASE_SERVICE_ROLE_KEY as string, {
     auth: {
         autoRefreshToken: false,
         persistSession: true
