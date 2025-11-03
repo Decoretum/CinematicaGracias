db1be2434b4ca00879ab5756c95e0f111fc5b6f1
Gael Estrera <gaelthegoodboy@gmail.com>
2025-11-04 05:06:24 +0800

    refactor: Modified home page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Box, Button } from "@mui/joy";
 import Typography from "@mui/material/Typography";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -15,8 +16,8 @@ export default function Home() {
 
       <Box className='md:ml-[5vw] md:mt-[3vh] md:w-[30vw] bg-black/30 p-6 rounded-lg text-white backdrop-blur-sm'>
         <Typography variant='body1'> 
-          As of 2075, there have been more than 2 million films released worldwide. 
-          Print media is extinct, and 79% of transcations, entertainment, and media are performed digitally.
+          Film is man's interpretation of the world which reflects mankind's inward desires and conflicting nature.
+          One of the beautiful capabilities of film is to unite the differing compasses each of us hold when navigating life's congenial journeys. 
           Cinematica Gracias's mission is to preserve and nurture the timeless masterpiece made by human creativity
           and ingenuinity for eternity.
         </Typography>
@@ -37,6 +38,26 @@ export default function Home() {
         <Button variant='soft' color='neutral'> Explore </Button>
       </Link>
     </Box>
+
+    <Box className='flex mt-[8vh] mr-auto ml-auto'>
+        <Box className='flex flex-row gap-7 md:ml-[5vw] md-ml-auto md:w-[30vw] bg-black/30 p-6 rounded-lg text-white backdrop-blur-sm'>
+          <Box className='flex flex-col gap-2'>
+            <Box>
+              <Typography variant='h5'>Ingmar Bergman</Typography>
+            </Box>
+            <Box> 
+              <Image className='rounded-lg' src='/ingar.jpeg' alt={""} width='345' height='460'  />
+            </Box>
+          </Box>
+          <Box>
+            <Typography variant='body1'> 
+            <i>“No art passes our conscience in the way film does, 
+              and goes directly to our feelings, deep down into the dark rooms of the soul.”</i>
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
     </>
   );
 }
